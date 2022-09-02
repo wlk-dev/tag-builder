5ef895fa9aadfd014d967ccf9ae27faf2f8f2d76
wlk <william.koelling@gmail.com>
2022-09-01 22:52:18 -0500

    refactored code, small bug fixes

--- assets/script.js
+++ assets/script.js
@@ -1,24 +1,21 @@
 function generateTag() {
-
     const formData = new FormData( document.getElementById('tag-form') );
-
     data = formatData(formData);
 
     document.getElementById('output-json').value = JSON.stringify(data['json'], undefined, 4);
     document.getElementById('output-tag').value = data['tag'];
-
 }
 
 function formatData( formData ) {
     formData.set("ctm_value", `${formData.get("ctm_value")} ${formData.get("ctm_values")}`);
     formData.delete("ctm_values");
 
-    var ctm_handle = formData.get("ctm_handle").replace(" ", "_").toLowerCase() ;
-    var uri = "http://" + ctm_handle + ".handle/erni?";
+    var ctm_handle = formData.get("ctm_handle").replaceAll(" ", "_").toLowerCase() ;
+    var base = `${ctm_handle}.handle/erni?`;
     var object = { "tag" : null, "json" : {} };
 
     formData.forEach(function(value, key){
-        if(key === "ctm_tags" || key === "ctm_value"){
+        if(key === "ctm_tags" || key === "ctm_value") {
             object['json'][key] = value.toLowerCase();
         } else {
             object['json'][key] = value.replaceAll(" ", "_").toLowerCase();
@@ -26,7 +23,7 @@ function formatData( formData ) {
     });
 
     object['json']['ctm_date'] = new Date().toISOString();
-    object["tag"] = uri += new URLSearchParams(object['json']).toString();
+    object["tag"] = base += new URLSearchParams(object['json']).toString();
 
     return object;
 }
