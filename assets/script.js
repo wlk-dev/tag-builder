6e77dcbcb253754475b5e9b47e6362a46240d8bf
wlk <william.koelling@gmail.com>
2022-09-01 22:04:53 -0500

    added javascript logic, updates css and html

---
+++ assets/script.js
@@ -0,0 +1,32 @@
+function generateTag() {
+
+    const formData = new FormData( document.getElementById('tag-form') );
+
+    data = formatData(formData);
+
+    document.getElementById('output-json').value = JSON.stringify(data['json'], undefined, 4);
+    document.getElementById('output-tag').value = data['tag'];
+
+}
+
+function formatData( formData ) {
+    formData.set("ctm_value", `${formData.get("ctm_value")} ${formData.get("ctm_values")}`);
+    formData.delete("ctm_values");
+
+    var ctm_handle = formData.get("ctm_handle").replace(" ", "_").toLowerCase() ;
+    var uri = "http://" + ctm_handle + ".handle/erni?";
+    var object = { "tag" : null, "json" : {} };
+
+    formData.forEach(function(value, key){
+        if(key === "ctm_tags" || key === "ctm_value"){
+            object['json'][key] = value.toLowerCase();
+        } else {
+            object['json'][key] = value.replace(" ", "_").toLowerCase();
+        }
+    });
+
+    object["tag"] = uri += new URLSearchParams(object['json']).toString();
+
+    return object;
+}
+
